fab710fc2b9f0831ab021c1218979e82e56f18e5
Matthieu PIERRE LOUIS <matthieu.pierrelouis@ext.canal-plus.com>
2026-09-08 18:45:41 +0200

    feat(query): add Query struct and constructor

--- query/query.go
+++ query/query.go
@@ -1,1 +1,15 @@
 package query
+
+type Query[T any] struct {
+	QueryId string
+	Query   func(args ...string) (T, error)
+	Args    []string
+}
+
+func NewQuery[T any](queryId string, query func(args ...string) (T, error), args ...string) *Query[T] {
+	return &Query[T]{
+		QueryId: queryId,
+		Query:   query,
+		Args:    args,
+	}
+}
