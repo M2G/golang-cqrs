a56b65ebea0061ebca72dae39210323489361123
Matthieu PIERRE LOUIS <matthieu.pierrelouis@ext.canal-plus.com>
2026-09-09 17:10:33 +0200

    feat(event): add event, event bus and event handler (async, channel-based)

--- event/event.go
+++ event/event.go
@@ -1,1 +1,13 @@
-package cmd
+package event
+
+type Event struct {
+	topic    string
+	Callback func(data interface{})
+}
+
+func NewEvent(topic string, callback func(data interface{})) *Event {
+	return &Event{
+		topic:    topic,
+		Callback: callback,
+	}
+}
--- event/event_handler.go
+++ event/event_handler.go
@@ -1,1 +1,33 @@
-package cmd
+package event
+
+type EventHandler struct {
+	channel chan Event
+	event   Event
+}
+
+func NewEventHandler(event Event, eventBus *EventBus) *EventHandler {
+	eh := &EventHandler{
+		channel: make(chan Event),
+		event:   event,
+	}
+
+	eventBus.Subscribe(event, eh)
+
+	go eh.listen()
+
+	return eh
+}
+
+func (eh *EventHandler) listen() {
+	for e := range eh.channel {
+		eh.event.Callback(e)
+	}
+}
+
+func (eh *EventHandler) Channel() chan Event {
+	return eh.channel
+}
+
+func (eh *EventHandler) Topic() string {
+	return eh.event.topic
+}
