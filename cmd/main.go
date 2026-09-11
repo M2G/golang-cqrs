24c8b9f71c390076fd02d997f0bbef094c4e299b
Matthieu PIERRE LOUIS <matthieu.pierrelouis@ext.canal-plus.com>
2026-09-11 17:24:22 +0200

    wip(facade): scaffold bank facade with event bus, NewFacade call incomplete

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,33 @@
 package main
+
+import (
+	"fmt"
+	"golang-cqrs/event"
+	"golang-cqrs/facade"
+)
+
+type BankAccount struct {
+	ID      string
+	Balance int
+}
+
+var accounts = &BankAccount{ID: "acc-1", Balance: 0}
+
+func NewBookFacade() *facade.Facade {
+	eventBus := event.NewEventBus()
+
+	balanceLogger := event.NewEventHandler(*event.NewEvent("BalanceUpdated", func(data interface{}) {
+		fmt.Printf("[EVENT] Notification abonné: solde actuel = %d centimes\n", accounts.Balance)
+	}), eventBus)
+
+	_ = balanceLogger
+
+	// cmd side
+
+	return facade.NewFacade(
+	// ...
+	)
+}
+
+func main() {
+}
