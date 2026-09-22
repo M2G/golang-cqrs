cc342295a75d2a16ff2f9443780d3c80fd9f0e43
Matthieu PIERRE LOUIS <matthieu.pierrelouis@ext.canal-plus.com>
2026-09-22 18:41:09 +0200

    feat(main): add pool

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"golang-cqrs/internal/config"
+	"golang-cqrs/internal/infrastructure/db"
+	"golang-cqrs/internal/infrastructure/logger"
 	"os"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
@@ -12,4 +17,14 @@ func main() {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
+
+	log := logger.New(cfg.LogLevel)
+	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGINT)
+	defer cancel()
+
+	pool, err := db.NewPool(ctx, cfg.DatabaseURL)
+	if err != nil {
+		log.WithError(err).Fatal("failed to connect to database")
+	}
+	defer pool.Close()
 }
