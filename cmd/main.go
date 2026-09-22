4634af25a844447bd27f3acfa76143669f91e4bf
Matthieu PIERRE LOUIS <matthieu.pierrelouis@ext.canal-plus.com>
2026-09-22 18:34:58 +0200

    feat(main): load conf

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"golang-cqrs/internal/config"
+	"os"
+)
 
+func main() {
+	cfg, err := config.Load()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
 }
