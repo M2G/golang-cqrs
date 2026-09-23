7abfdc60994dd7a1490eae612f4a8f89194a4f12
Matthieu PIERRE LOUIS <matthieu.pierrelouis@ext.canal-plus.com>
2026-09-23 03:12:24 +0200

    feat(main): add repo

--- cmd/main.go
+++ cmd/main.go
@@ -4,27 +4,38 @@ import (
 	"context"
 	"fmt"
 	"golang-cqrs/internal/config"
+	"golang-cqrs/internal/cqrs"
 	"golang-cqrs/internal/infrastructure/db"
 	"golang-cqrs/internal/infrastructure/logger"
+	"golang-cqrs/internal/infrastructure/repository"
+	"golang-cqrs/internal/job"
 	"os"
 	"os/signal"
 	"syscall"
+
+	"github.com/sirupsen/logrus"
 )
 
 func main() {
+	// configuration
 	cfg, err := config.Load()
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
-
+	// logger
 	log := logger.New(cfg.LogLevel)
 	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGINT)
 	defer cancel()
 
+	// database
 	pool, err := db.NewPool(ctx, cfg.DatabaseURL)
 	if err != nil {
 		log.WithError(err).Fatal("failed to connect to database")
 	}
 	defer pool.Close()
+
+	// repository
+	repo := repository.New(pool)
+
 }
